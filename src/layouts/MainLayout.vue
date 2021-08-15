512ea17672f740aa9e42692a156fac4a2f076ed4
vanjkes <shyian.ivan@yahoo.com>
2021-08-15 23:01:14 +0300

    Fixed scroll problem on mobile devices

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,12 +10,22 @@
 <script>
 import Navbar from '@/components/Navbar'
 import UserAuth from '@/components/UserAuth'
-import {defineComponent, onMounted} from 'vue'
+import {defineComponent, onMounted, nextTick} from 'vue'
+import detectMob from '@/utils/isMobile'
 
 export default defineComponent({
   components: {
     Navbar,
     UserAuth
+  },
+  setup() {
+    onMounted(async() => {
+      await nextTick()
+      const isMobile = detectMob()
+      if (isMobile) {
+        document.documentElement.scrollTo({top: 0})
+      }
+    })
   }
 })
 </script>
